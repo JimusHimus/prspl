97523b0d9d8a9018ea20154884e1eb3beef35252
Георгий <georgii123456789@yandex.ru>
2021-07-22 01:02:44 +0300

    Initial commit

---
+++ prspl/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for prspl project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prspl.settings')
+
+application = get_asgi_application()
